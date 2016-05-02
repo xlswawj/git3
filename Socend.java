fad85d49394211b59d628b67009f4f0f129d8a2a
xlswawj <1932393653@qq.com>
2016-05-02 17:33:59 +0800

    ‘4.30’

---
+++ Socend.java
@@ -0,0 +1,11 @@
+package  com.briup.ch03;
+ 
+public class Socend{
+   public  static void showHello(String name){
+	 System.out.println("hello"+name);
+        }
+   public static void main(String[] args){
+      showHello ("уехЩ") ;   
+   
+ }
+}
